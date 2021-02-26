d4c842b91bfcc964696ca6b62b46c0f6d85ae5b1
Wentao Shi <wentao771@gmail.com>
2021-02-25 19:54:17 -0600

    gpu

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@
 #PBS -A hpc_michal01
 #PBS -j oe
 
-cd /work/derick/drug-gen-project/DeepDruGen/
+cd /work/derick/molecule-generator-project/Molecule-RNN/
 
 singularity exec --nv -B /work,/project,/usr/lib64 /work/derick/singularities/pytorch-and-others.simg python train.py
