03b2c0f1b7d08494bc4982797397e475d850af27
Wentao Shi <wentao771@gmail.com>
2021-04-01 10:26:23 -0500

    updated to support selfies

--- vocab/chembl_selfies_vocab.py
+++ vocab/chembl_selfies_vocab.py
@@ -1,63 +1,34 @@
-"""generate the vocabulary accorrding to the regular expressions of
-SMILES of molecules."""
+"""
+Generate the vocabulary of the selfies of the smiles in the dataset
+"""
 import yaml
-from os import listdir
-from os.path import isfile, join
-import re
+import selfies as sf
+from tqdm import tqdm
 
 
-def read_smiles_file_zinc(path, percentage):
+def read_smiles_file(path, percentage):
     with open(path, 'r') as f:
-        smiles = [line.split(' ')[0] for line in f.readlines()[1:]]
+        smiles = [line.strip("\n") for line in f.readlines()]
     num_data = len(smiles)
     return smiles[0:int(num_data * percentage)]
 
 
-def replace_halogen(string):
-    """Regex to replace Br and Cl with single letters"""
-    br = re.compile('Br')
-    cl = re.compile('Cl')
-    string = br.sub('R', string)
-    string = cl.sub('L', string)
-
-    return string
-
-
-def tokenize(smiles):
-    """Takes a SMILES string and returns a list of tokens.
-    This will swap 'Cl' and 'Br' to 'L' and 'R' and treat
-    '[xx]' as one token."""
-    regex = '(\[[^\[\]]{1,6}\])'
-    smiles = replace_halogen(smiles)
-    char_list = re.split(regex, smiles)
-    tokenized = []
-    for char in char_list:
-        if char.startswith('['):
-            tokenized.append(char)
-        else:
-            chars = [unit for unit in char]
-            [tokenized.append(unit) for unit in chars]
-    return tokenized
-
-
 if __name__ == "__main__":
-    dataset_dir = "../../zinc-smiles/"
-    output_vocab = "./regex_vocab.yaml"
+    dataset_path = "../../chembl-data/chembl_28/chembl_28_sqlite/chembl28-cleaned.smi"
+    output_vocab = "./chembl_selfies_vocab.yaml"
 
-    smiles_files = [f for f in listdir(
-        dataset_dir) if isfile(join(dataset_dir, f))]
-    all_tokens = []
-    for i, f in enumerate(smiles_files):
-        smiles = read_smiles_file_zinc(dataset_dir + f, 1)
-        tokens = []
-        [tokens.extend(tokenize(x)) for x in smiles]
-        all_tokens.extend(tokens)
-        print('{} out of {} files processed.'.format(i, len(smiles_files)))
+    smiles = read_smiles_file(dataset_path, 1)
+    selfies = []
+    for x in tqdm(smiles):
+        x = sf.encoder(x)
+        if x is not None:
+            selfies.append(x)
 
-    all_tokens = set(all_tokens)
+    print('getting alphabet from selfies...')
+    vocab = sf.get_alphabet_from_selfies(selfies)
 
     vocab_dict = {}
-    for i, token in enumerate(all_tokens):
+    for i, token in enumerate(vocab):
         vocab_dict[token] = i
 
     i += 1
